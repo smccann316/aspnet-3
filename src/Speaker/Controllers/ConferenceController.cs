50c60ed5f58fa727e8772137cd4f8b27d35881ca
Steve <Steve.mccann@live.com>
2019-09-12 14:17:58 -0400

    added base files tyo project

---
+++ src/Speaker/Controllers/ConferenceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Speaker.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Speaker.Controllers
+{
+    public class ConferenceController : Controller 
+    {
+        public ConferenceController(IConferenceService service)
+        {
+
+        }
+    }
+}
